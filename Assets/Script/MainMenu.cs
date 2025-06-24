1318ce3b90616da929051a29c59db92fd5a1ca7c
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-06-24 21:42:39 +0700

    Add Setting Scene (#9)

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -6,15 +6,23 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        // Load the game scene (assuming it's named "GameScene")
         SceneManager.LoadSceneAsync(1);
     }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
     public void QuitGame()
     {
-        // Quit the application
         Application.Quit();
 
-        // If running in the editor, stop playing
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
