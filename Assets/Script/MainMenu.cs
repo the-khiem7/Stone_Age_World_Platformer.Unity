e7372fbd5bd1c8d749bd0042c51ac561baf802e0
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-06-24 19:21:15 +0700

    Complete the Menu (#3)

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -1,3 +1,4 @@
+#define UNITY_EDITOR
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,5 +8,15 @@ public class MainMenu : MonoBehaviour
     {
         // Load the game scene (assuming it's named "GameScene")
         SceneManager.LoadSceneAsync(1);
+    }
+    public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
+
+        // If running in the editor, stop playing
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
